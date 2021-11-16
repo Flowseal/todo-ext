bf4b5be51768241a3525eeae8af022ea98b16509
rawr111 <70286254+rawr111@users.noreply.github.com>
2021-11-16 20:07:00 +0300

    Возможность добавления новых todo

--- popup.js
+++ popup.js
@@ -1,13 +1,32 @@
 var ext = chrome.extension.getBackgroundPage();
+var todos = [];
+var newTodoInput = $('#newTodoInput');
+var newTodoBtn = $('#newTodoBtn');
 
-var input = $('#todoCreationInput');
-var btn = $('#todoCreationBtn');
-var todos = $('#todos');
+newTodoBtn.on('click', ()=>{
+    if (!!!newTodoInput.val()) return;
+    var todo = new Todo(newTodoInput.val());
+    todo.draw();
+    saveTodo(todo);
+});
 
-var todo = new Todo('aboba');
-todo.draw();
-todo.draw();
-todo.draw();
-todo.draw();
-todo.draw();
-todo.draw();
+function getTodos(callback){
+    ext.get('todos', callback);
+}
+function saveTodo(todo){
+    todos.push(todo);
+    ext.set({'todos':todos});
+}
+function deleteTodo(todo){
+    todos
+}
+
+getTodos((data)=>{
+    for (var i of data.todos){
+        var todo = new Todo(i.text);
+        todo.draw();
+        todos.push(todo);
+    }
+});
+//var todo = new Todo('абобик');
+//saveTodo(todo);
--- todo.js
+++ todo.js
@@ -4,14 +4,16 @@ class Todo{
         this.time = new Date();
         this.important = false;
         this.urgently = false;
+        this.el = $(``)
     }
     draw(){
         var html = document.getElementById('todoTemplate').innerHTML;
         var template = Handlebars.compile(html);
-        var data = template({ text:this.text, time:this.time });
+        console.log()
+        var data = template({ text:this.text, time:date.format(this.time, 'MM dd hh:mm') });
         document.getElementById(`todos`).insertAdjacentHTML("beforeend", data);
     }
     delete(){
-
+        
     }
 }
